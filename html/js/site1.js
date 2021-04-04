ba8b5a3f8cac764b0acc15bc5fcd3b31a7d1a3ab
neporoma <neporoma@yandex.ru>
2021-04-04 15:54:22 +0300

    Update site1.js

--- html/js/site1.js
+++ html/js/site1.js
@@ -3,7 +3,7 @@ function clockTimer()
   var date = new Date();
   
   var time = [date.getHours(),date.getMinutes(),date.getSeconds()]; // |[0] = Hours| |[1] = Minutes| |[2] = Seconds|
-  var dayOfWeek = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"]
+  var dayOfWeek = ["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"]
   var days = date.getDay();
   
   if(time[0] < 10){time[0] = "0"+ time[0];}
